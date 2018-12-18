5d644f52b0921fdb971caae9047acd4c2d33a6ff
Travis Lang <tlang50@icloud.com>
2018-12-18 15:41:07 -0600

    add edit username functionality

--- src/App.js
+++ src/App.js
@@ -1,67 +1,77 @@
 import React, { Component } from 'react';
 
 const getCookie = (cookieName) => {
-  // Get name followed by anything except a semicolon
-  const cookieString = RegExp(''+cookieName+'[^;]+').exec(document.cookie);
-  // Return everything after the equal sign, or an empty string if the cookie name not found
-  return decodeURIComponent(!!cookieString ? cookieString.toString().replace(/^[^=]+./,'') : '');
+    // Get name followed by anything except a semicolon
+    const cookieString = RegExp('' + cookieName + '[^;]+').exec(document.cookie);
+    // Return everything after the equal sign, or an empty string if the cookie name not found
+    return decodeURIComponent(!!cookieString ? cookieString.toString().replace(/^[^=]+./, '') : '');
 }
 
 class App extends Component {
-  state = {
-    clickCount: getCookie('count') || 0,
-    usernameIsEditable: false,
-  }
+    state = {
+        clickCount: getCookie('count') || 0,
+        usernameIsEditable: false,
+    }
 
-  handleClick = () => {
-    const newCount = Number(this.state.clickCount) + 1;
-    document.cookie = `count=${newCount}`;
-    this.setState({
-      clickCount: newCount,
-    });
-  }
+    handleClick = () => {
+        const newCount = Number(this.state.clickCount) + 1;
+        document.cookie = `count=${newCount}`;
+        this.setState({
+            clickCount: newCount,
+        });
+    }
 
-  editUsername = () => {
-    this.setState({
-      usernameIsEditable: true,
-    });
-  }
+    editUsername = () => {
+        this.setState({
+            usernameIsEditable: true,
+        });
+    }
 
-  saveUsername = () => {
-    this.setState({
-      usernameIsEditable: false,
-    });
-  }
+    saveUsername = () => {
+        this.setState({
+            usernameIsEditable: false,
+        });
+    }
 
-  render() {
-    return (
-      <div>
-        <center>
-          <h1>Click the Cookie!!</h1>
-          <p>
-            Username:
-            {/* Username should go here */}
-            {/* The next block of code is conditional rendering.
+    handleChange = ({target}) => {
+        this.setState({
+            username: target.value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <center>
+                    <h1>Click the Cookie!!</h1>
+                    <p>
+                        Username: 
+             {this.state.username}
+                        {/* The next block of code is conditional rendering.
             Look at the documentation https://reactjs.org/docs/conditional-rendering.html
             if this is new to you. */}
-            {this.state.usernameIsEditable ?
-              <button onClick={this.saveUsername}>Save Username</button> :
-              <button onClick={this.editUsername}>Edit Username</button>
-            }
-          </p>
-          <p>{this.state.clickCount}</p>
-          <span
-            role="img"
-            aria-label="cookie"
-            style={{fontSize: '100px', cursor: 'pointer'}}
-            onClick={this.handleClick}
-          >
-            🍪
+                        {this.state.usernameIsEditable ?
+                            <div>
+                                <input value={this.state.username} onChange={this.handleChange} />
+                                <button onClick={this.saveUsername}>Save Username</button>
+                            </div>
+                             :
+                            <button onClick={this.editUsername}>Edit Username</button>
+                        }
+                    </p>
+                    <p>{this.state.clickCount}</p>
+                    <span
+                        role="img"
+                        aria-label="cookie"
+                        style={{ fontSize: '100px', cursor: 'pointer' }}
+                        onClick={this.handleClick}
+                    >
+                        🍪
           </span>
-        </center>
-      </div>
-    );
-  }
+                </center>
+            </div>
+        );
+    }
 }
 
 export default App;
